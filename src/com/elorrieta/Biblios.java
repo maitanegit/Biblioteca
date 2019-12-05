e13a1708da1c1068713782fa39523d82a38851e0
maitane <irakaslemai@gmail.com>
2019-12-05 17:08:17 +0100

    Biblio-main menu principal realizado falta implementar opciones

--- src/com/elorrieta/Biblios.java
+++ src/com/elorrieta/Biblios.java
@@ -1,8 +1,35 @@
 package com.elorrieta;
 
+import java.util.Scanner;
+
 public class Biblios {
 
 	public static void main(String[] args) {
 		System.out.println("Inicio del proyecto"); //syso ctrl espacio enter
+		
+		System.out.println("Introduce la operación a realizar:");
+		System.out.println("A. Tomar prestado un libro");
+		System.out.println("B. Devolver libro");
+		System.out.println("S. Salir");
+		System.out.println("");
+		
+		Scanner sc = new Scanner(System.in);
+		char op = sc.nextLine().toUpperCase().charAt(0);
+		
+		switch (op) {
+		case 'A':
+			
+			break;
+		case 'B':
+			
+			break;
+		case 'S':
+	
+			break;
+		default:
+			break;
+		}
+				
+		
 	}
 }
